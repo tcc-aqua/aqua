5ad16c683221aa15641f6879695df24105bce21d
davithekid <byjdrk.27@gmail.com>
2025-09-25 08:01:12 -0300

    feat: server basic

---
+++ backend-dash/src/app.js
@@ -0,0 +1,14 @@
+import fastify from 'fastify';
+const app = fastify({
+    logger: {
+        transport:{
+            target: 'pino-pretty'
+        }
+    }
+});
+
+app.get("/", (request, reply) => {
+    return reply.status(200).send({message: 'Hello API!!'});
+})
+
+export default app;
---
+++ backend-dash/src/server.js
@@ -0,0 +1,19 @@
+import app from "./app.js";
+import { connectDB } from "./config/database.js";
+
+const PORT = process.env.PORT || 3333;
+
+const start = async () => {
+    try {
+        await connectDB;
+        await app.listen({
+            host: '0.0.0.0', port: PORT
+        })
+        console.log(`Server listening on ${PORT}`);
+    } catch (error){
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+start();
