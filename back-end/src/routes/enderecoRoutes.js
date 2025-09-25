e530ecb4dcf74bd8b3ffeb724dbca2d3f000414b
davithekid <byjdrk.27@gmail.com>
2025-09-25 07:42:33 -0300

    refactory

--- back-end/src/routes/enderecoRoutes.js
+++
@@ -1,9 +0,0 @@
-import EnderecoController from "../controllers/EnderecoController.js";
-
-export default async function userRoutes(app) {
-    app.get("/enderecos", EnderecoController.getAll);
-    app.post("/enderecos", EnderecoController.create);
-    app.get("/enderecos/:id", EnderecoController.getByid);
-    app.put("/enderecos/:id", EnderecoController.update);
-    app.delete("/enderecos/:id", EnderecoController.delete);
-}
--- back-end/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import UserController from "../controllers/UserController.js";
-
-export default async function userRoutes(app) {
-    app.get("/users", UserController.getAll);
-    app.post("/users", UserController.create);
-    app.get("/users/:id", UserController.getByid);
-    app.put("/users/:id", UserController.update);
-    app.delete("/users/:id", UserController.delete);
-}
