bca669b0673844a3045a4e216b8f78e38885f281
davithekid <byjdrk.27@gmail.com>
2025-10-20 14:02:28 -0300

    feat: sql by docker

--- back-end/src/config/sequelize.js
+++ back-end/src/config/sequelize.js
@@ -4,21 +4,31 @@ dotenv.config({ path: resolve("..", ".env") });
 
 import { Sequelize } from "sequelize";
 
+// escolhe host e porta com base no ambiente
+const dbHost = process.env.DB_ENV === "docker" 
+    ? process.env.DB_HOST_DOCKER 
+    : process.env.DB_HOST_LOCAL;
+
+const dbPort = process.env.DB_ENV === "docker" 
+    ? Number(process.env.DB_PORT_DOCKER)
+    : Number(process.env.DB_PORT_LOCAL);
+
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASS,
     { 
-        host: process.env.DB_HOST,
+        host: dbHost,
+        port: dbPort,
         dialect: process.env.DB_DIALECT || 'mysql',
         logging: false
-    })
+    }
+);
 
 export const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    console.log('Conexão estabelecida com sucesso!!.');
-
+    console.log('Conexão estabelecida com sucesso!!');
     await sequelize.sync(); 
     console.log('Tabelas sincronizadas com o banco!');
   } catch (error) {
