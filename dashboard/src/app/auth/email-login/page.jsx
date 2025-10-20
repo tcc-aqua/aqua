f23d4de3602ed12e6ce8ba3f635ae5414f5f80fc
carolrossi <anacarollinirrosi@gmail.com>
2025-10-20 11:18:56 -0300

    feat: login

--- dashboard/src/app/auth/email-login/page.jsx
+++ dashboard/src/app/auth/email-login/page.jsx
@@ -6,36 +6,58 @@ import Link from "next/link";
 
 export default function EmailLogin() {
     return (
-        <div className="grid min-h-svh lg:grid-cols-3 ">
+        <div className="grid min-h-screen lg:grid-cols-2">
 
-            <div className="relative hidden lg:block bg-[#0372c6]">
-                <img
-                    src="/logo-png.svg"
-                    alt="Imagem ilustrativa"
-                    className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-                />
-                <div className="absolute inset-0 flex flex-col justify-end items-center pb-10 text-white text-center">
+            <div className="relative hidden lg:flex flex-col bg-[#0372c6]">
 
-                    <div className="text-4xl font-semibold leading-tight drop-shadow-lg p-8">
-                        Organize suas leituras com o Skoob!
+                <div className="relative flex-grow">
+                    <img
+                        src="/logo-png.svg"
+                        alt="Imagem ilustrativa"
+                        className="absolute inset-0 h-full w-full object-cover"
+                    />
+
+                    <div className="absolute inset-0 flex flex-col justify-end items-center pb-10 px-4 text-white z-20">
+                        <h1 className="text-4xl md:text-4xl lg:text-6xl font-bold">
+                        Bem-vindo ao Sistema Aqua! 
+                        </h1>
                     </div>
                 </div>
+
             </div>
-            <div className="flex flex-col justify-center p-6 md:p-10">
 
-                <div>
+            <div className="flex flex-col p-6 md:p-10 h-full">
 
-                    <Link href='../login' className="text-[#0372c6] font-bold">
-                        {'< '} Voltar
-                    </Link>
+
+                <div className="flex items-center gap-2 mb-6">
+                    <img src="/logo.svg" className="w-12" />
+                    <img src="/escrita.png" className="w-18 mt-2" />
                 </div>
-                <h1 className="text-2xl font-bold">Bom te ver novamente!</h1>
-                <h2 className="text-sm mb-6">Preencha o seu e-mail para acessar sua conta.</h2>
 
-                <div className="w-full max-w-lg">
-                    <InputWithAdornmentDemo />
+                <div className="flex flex-col justify-center flex-grow">
+
+                    <div className="mb-4">
+                        <Link href="../login" className="text-[#0372c6] font-bold flex justify-center">
+                            {'< '} Voltar
+                        </Link>
+                    </div>
+
+                    <div className="">
+                        <h1 className="flex justify-center text-3xl font-bold mb-2">Bom te ver novamente!</h1>
+                        <h2 className="text-md mb-6 flex justify-center">Preencha o seu e-mail para acessar sua conta.</h2>
+
+                        <div className="flex justify-center">
+                            <div className="w-full max-w-lg flex justify-center ">
+                                <InputWithAdornmentDemo />
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
+
             </div>
+
         </div>
     );
 }
+
--- dashboard/src/components/input-07.jsx
+++ dashboard/src/components/input-07.jsx
@@ -14,22 +14,22 @@ export default function InputWithAdornmentDemo() {
   };
 
   return (
-    <div className="w-full max-w-xs space-y-2">
+    <div className="w-full max-w-xs space-y-2 ">
       <div
-        className="relative flex items-center rounded-md border focus-within:ring-1 focus-within:ring-ring pl-2">
+        className="relative flex items-center rounded-full border focus-within:ring-1  focus-within:ring-ring pl-2">
         <MailIcon className="h-5 w-5 text-muted-foreground" />
         <Input
           type="email"
           placeholder="Email"
-          className="border-0 focus-visible:ring-0 shadow-none" />
+          className="border-0 focus-visible:ring-0 shadow-none h-15 rounded-full " />
       </div>
       <div
-        className="relative flex items-center rounded-md border focus-within:ring-1 focus-within:ring-ring px-2">
+        className="relative flex items-center rounded-full border focus-within:ring-1 focus-within:ring-ring px-2">
         <LockIcon className="h-5 w-5 text-muted-foreground" />
         <Input
           type={showPassword ? "text" : "password"}
           placeholder="Password"
-          className="border-0 focus-visible:ring-0 shadow-none" />
+          className="border-0 focus-visible:ring-0 shadow-none h-15 rounded-full" />
         <button onClick={togglePasswordVisibility}>
           {showPassword ? (
             <EyeOffIcon className="h-5 w-5 text-muted-foreground" />
@@ -39,9 +39,9 @@ export default function InputWithAdornmentDemo() {
         </button>
       </div>
       <Link href={'../dashboard'}>
-      <Button className="w-full">Entrar</Button>
+      <Button className="w-full rounded-full h-10 mt-5">Entrar</Button>
       </Link>
-      <Link href='../login' className="text-[#0372c6] font-bold flex justify-center">
+      <Link href='../login' className="text-[#0372c6] underline hover:text-blue-900 flex justify-center">
          Esqueci minha senha
       </Link>
     </div>
