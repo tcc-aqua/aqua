c14955576711c27b02da007e17968aa4b01f82a6
davithekid <byjdrk.27@gmail.com>
2025-10-20 16:48:15 -0300

    feat: metodos de usuário completo

--- back-end/src/controllers/UserController.js
+++ back-end/src/controllers/UserController.js
@@ -1,4 +1,23 @@
 import UserService from "../services/UserService.js";
+import { z } from 'zod';
+
+const createUserSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    cpf: z.string().length(14),
+    password: z.string().min(6),
+    role: z.enum(['morador', 'sindico']).optional(),
+})
+
+const updateUserSchema = z.object({
+    name: z.string().optional(),
+    email: z.string().email().optional(),
+    cpf: z.string().length(14).optional(),
+    password: z.string().min(6).optional(),
+    role: z.enum(['morador', 'sindico']).optional(),
+});
+
+const idSchema = z.string().uuid();
 
 export default class UserController {
 
@@ -21,6 +40,24 @@ export default class UserController {
         const { id } = req.params;
         const user = await UserService.getUserById(id);
         reply.send(user)
+    }
+
+    static async create(req, reply) {
+        const validateUser = createUserSchema.parse(req.body);
+        const user = await UserService.createUser(validateUser);
+        reply.status(201).send(user);
+    }
+
+    static async update(req, reply) {
+        const validatedUser = updateUserSchema.parse(req.body);
+        const updateUser = await UserService.updateUser(id, validatedUser);
+        reply.send(updateUser);
+    }
+
+    static async deactivate(req, reply) {
+        const { id } = idSchema.parse(req.params);
+        const user = await UserService.deactivateUser(id);
+        reply.status(200).send(user)
     }
 
 }
