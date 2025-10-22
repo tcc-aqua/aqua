a8a879059b46c17a3839d43965e9d4483ca9af37
davithekid <byjdrk.27@gmail.com>
2025-10-22 13:32:30 -0300

    fix routes

--- back-end/src/routes/condominio.routes.js
+++ back-end/src/routes/condominio.routes.js
@@ -3,8 +3,8 @@ import CondominioController from "../controllers/CondominioController.js";
 export default async function condominioRoutes(fastify){
 
     fastify.get('/', CondominioController.getAll);
-    fastify.get('/actives', CondominioController.getAllActives);
-    fastify.get('/deactivates', CondominioController.getAllInativos);
+    fastify.get('/ativos', CondominioController.getAllActives);
+    fastify.get('/inativos', CondominioController.getAllInativos);
     fastify.post('/', CondominioController.create);
     fastify.put('/:id', CondominioController.update);
     fastify.put('/inativar/:id', CondominioController.inativar);
--- back-end/src/routes/user.routes.js
+++ back-end/src/routes/user.routes.js
@@ -3,11 +3,11 @@ import UserController from "../controllers/UserController.js";
 export default async function userRoutes(fastify) {
 
     fastify.get('/', UserController.getAll);
-    fastify.get('/actives', UserController.getAllActives);
-    fastify.get('/deactivates', UserController.getAllDeactivated);
+    fastify.get('/ativos', UserController.getAllActives);
+    fastify.get('/inativos', UserController.getAllDeactivated);
     fastify.get('/:id', UserController.getById);
     fastify.put('/:id', UserController.update);
     fastify.post('/', UserController.create);
-    fastify.put('/deactivate/:id', UserController.deactivate);
+    fastify.put('/inativar/:id', UserController.deactivate);
 
 }
