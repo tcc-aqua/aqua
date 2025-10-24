31bbb1114ca43513bf3f424c48ef4acd8e5c5e54
davithekid <byjdrk.27@gmail.com>
2025-10-24 15:50:04 -0300

    feat: model leitura de sensor

---
+++ backend-dash/src/models/LeituraSensor.js
@@ -0,0 +1,34 @@
+import { Model, DataTypes } from "sequelize";
+import sequelize from "../config/sequelize.js";
+import Sensor from "./Sensor";
+
+export default class LeituraSensor extends Model {}
+
+LeituraSensor.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    sensor_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {model: Sensor, key: 'id'}
+    },
+    consumo: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    },
+    vazamento_detectado: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+    data_registro: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    }
+}, {
+    sequelize,
+    tableName: 'leituras_sensores',
+    timestamps: false
+})
