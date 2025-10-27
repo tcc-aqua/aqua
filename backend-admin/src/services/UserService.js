052f4008faafbc2361b645de639f4de0cf554465
davithekid <byjdrk.27@gmail.com>
2025-10-27 10:52:43 -0300

    fix filtro get all

--- backend-admin/src/services/UserService.js
+++ backend-admin/src/services/UserService.js
@@ -1,3 +1,5 @@
+import Apartamento from "../models/Apartamento.js";
+import Casa from "../models/Casa.js";
 import User from "../models/User.js";
 
 export default class UserService {
@@ -5,19 +7,47 @@ export default class UserService {
     // fazendo paginação de usuários para não sobracarregar o front-end com muitos dados
     static async getAllUsers(page = 1, limit = 10) {
         try {
-            const options = {
+            const result = await User.paginate({
                 page,
                 paginate: limit,
                 order: [['criado_em', 'DESC']],
-                attributes: { exclude: ['password'] }
-            }
-            const users = await User.paginate(options);
-            return users;
+                attributes: { exclude: ['password'] },
+                include: [
+                    { model: Casa, as: 'casa' },
+                    { model: Apartamento, as: 'apartamento' }
+                ]
+            });
+
+            const usersWithResidencia = result.docs.map(user => {
+                let residencia = null;
+
+                if (user.residencia_type === 'casa' && user.casa) {
+                    residencia = `${user.casa.logradouro}, ${user.casa.numero} - ${user.casa.bairro}, ${user.casa.cidade} - ${user.casa.uf}`;
+                } else if (user.residencia_type === 'apartamento' && user.apartamento) {
+                    residencia = `Bloco ${user.apartamento.bloco} - Apt ${user.apartamento.numero}, Cond.: ${user.apartamento.condominio_id}`;
+                }
+
+                const plainUser = user.get({ plain: true });
+                delete plainUser.casa;
+                delete plainUser.apartamento;
+
+                return {
+                    ...plainUser,
+                    residencia
+                };
+            });
+
+            return {
+                ...result,
+                docs: usersWithResidencia
+            };
         } catch (error) {
             console.error('Erro ao buscar usuários', error);
             throw error;
         }
     }
+
+
 
     static async getAllUserActives(page = 1, limit = 10) {
         try {
