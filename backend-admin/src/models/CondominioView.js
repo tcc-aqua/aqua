45a8bbf5cff05fed813a5ce36a847198ef82e8ea
davithekid <byjdrk.27@gmail.com>
2025-10-29 09:07:43 -0300

    feat: endereco nas views

--- backend-admin/src/models/CondominioView.js
+++ backend-admin/src/models/CondominioView.js
@@ -12,24 +12,59 @@ CondominioView.init(
     },
     condominio_nome: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     condominio_codigo: {
-      type: DataTypes.CHAR(10),
+      type: DataTypes.STRING(10),
+      allowNull: false,
+    },
+    logradouro: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    numero: {
+      type: DataTypes.STRING(10),
+      allowNull: true,
+    },
+    bairro: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    cidade: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    uf: {
+      type: DataTypes.STRING(2),
+      allowNull: false,
+    },
+    cep: {
+      type: DataTypes.STRING(9),
+      allowNull: true,
+    },
+    endereco_completo: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     data_criacao: {
       type: DataTypes.DATE,
+      allowNull: false,
     },
     numero_apartamentos: {
       type: DataTypes.INTEGER,
+      allowNull: true,
     },
     numero_sensores: {
       type: DataTypes.INTEGER,
+      allowNull: true,
     },
     sindico_nome: {
       type: DataTypes.STRING,
+      allowNull: true,
     },
     condominio_status: {
       type: DataTypes.ENUM("ativo", "inativo"),
+      allowNull: false,
     },
   },
   {
