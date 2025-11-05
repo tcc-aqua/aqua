f9508242b7c98613a919f2519328d2b736745cee
davithekid <byjdrk.27@gmail.com>
2025-11-05 11:02:51 -0300

    fix

--- backend-admin/src/routes/suporte.routes.js
+++ backend-admin/src/routes/suporte.routes.js
@@ -1,7 +1,13 @@
 import SuporteController from "../controllers/SuporteController.js";
 
-export default async function suporteRoutes(fastify){
+export default async function suporteRoutes(fastify) {
 
-    fastify.get('/', SuporteController.getAll);
-
+    fastify.get('/',
+        {
+            schema: {
+                summary: 'Listando todas as mensagens de clientes para a administração',
+                tags: ['suporte'],
+                description: 'Listando todas as mensagens'
+            }
+        }, SuporteController.getAll);
 }
