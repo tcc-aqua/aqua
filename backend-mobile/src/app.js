904eebc3ec1d0377f71ea6cec0c0b109907fed1e
davithekid <byjdrk.27@gmail.com>
2025-11-11 15:18:46 -0300

    feat: docs do mobile

--- backend-mobile/src/app.js
+++ backend-mobile/src/app.js
@@ -1,6 +1,11 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
 import fastifyFormbody from '@fastify/formbody';
+import { fastifySwagger } from '@fastify/swagger';
+import swaggerUI from '@fastify/swagger-ui';
+import pino from 'pino'
+import fs from 'fs'
+
 import authRoutes from './routes/auth.routes.js';
 import apartamentoRoutes from './routes/apartamento.routes.js';
 import dicaRoutes from './routes/dica.routes.js';
@@ -8,10 +13,28 @@ import userRoutes from './routes/user.routes.js';
 import passwordRoutes from './routes/password.routes.js';
 import cepRoutes from './routes/cep.routes.js';
 import metasRoutes from './routes/metas.routes.js';
-// ==================================================================
-// 1. IMPORTA O ARQUIVO DE ROTAS QUE CRIAMOS
-// ==================================================================
 import profileRoutes from './routes/profile.routes.js';
+
+if (!fs.existsSync('./logs')) fs.mkdirSync('./logs')
+
+// cria log diário
+const date = new Date().toISOString().slice(0, 10)
+const filePath = `./logs/${date}.log`
+const fileStream = fs.createWriteStream(filePath, { flags: 'a' })
+
+const prettyTransport = pino.transport({
+    target: 'pino-pretty',
+    options: {
+        colorize: true,
+        translateTime: 'SYS:standard',
+        ignore: 'pid,hostname',
+    },
+})
+
+const multiStream = pino.multistream([
+    { stream: prettyTransport },
+    { stream: fileStream },
+])
 
 const fastify = Fastify({
     logger: {
@@ -29,9 +52,42 @@ await fastify.register(cors, {
 
 fastify.register(fastifyFormbody);
 
-fastify.get('/', (req, reply) => {
-    return reply.status(200).send('Hello API MOBILE!');
+// docs api
+await fastify.register(fastifySwagger, {
+    openapi: {
+        info: {
+            title: 'Aqua API Mobile',
+            version: '1.0.0',
+            description: 'Documentação da nossa API Mobile do Sistema Aqua.'
+        },
+    }
 });
+
+await fastify.register(swaggerUI, {
+    routePrefix: '/docs',
+    uiConfig: {
+        docExpansion: 'list',
+        deepLinking: false
+    },
+    initOAuth: {},
+});
+
+fastify.get('/api', {
+    schema: {
+        tags: ['Health Check'],
+        summary: 'Status da API',
+        description: 'Verifica se a API está online e respondendo',
+        response: {
+            200: {
+                type: 'string',
+                example: 'Hello API'
+            }
+        }
+    }
+}, (req, reply) => {
+    return reply.status(200).send('Hello Mobile!')
+})
+
 
 fastify.register(authRoutes, {prefix: '/api/auth'});
 fastify.register(apartamentoRoutes, {prefix: '/api/apartamentos'});
--- backend-mobile/src/routes/auth.routes.js
+++ backend-mobile/src/routes/auth.routes.js
@@ -2,7 +2,33 @@ import UserController from "../controllers/AuthController.js";
 import { authMiddleware } from "../middlewares/AuthMidlleweare.js"
 
 export default async function authRoutes(fastify) {
-  fastify.post("/register", UserController.register);
-  fastify.post("/login", UserController.login);
-  fastify.get('/me', { preHandler: authMiddleware }, UserController.me);
+  fastify.post("/register", 
+    {
+      schema: {
+        summary: 'Cadastrando um novo usuário',
+        tags: ['autenticação'],
+        description: 'Registrando um novo usuário no sistema mobile'
+      }
+    },
+    UserController.register);
+
+  fastify.post("/login",
+    {
+      schema: {
+        summary: 'Login do usuário',
+        tags: ['autenticação'],
+        description: 'Rota para o usuário poder fazer o login e entrar no seu app'
+      }
+    },
+    UserController.login);
+
+  fastify.get('/me', 
+    {
+      schema: {
+        summary: "Informações do usuário",
+        tags: ['autenticação'],
+        description: 'Rota para a API puxar as informações do usuário logado'
+      }
+    },
+    { preHandler: authMiddleware }, UserController.me);
 }
