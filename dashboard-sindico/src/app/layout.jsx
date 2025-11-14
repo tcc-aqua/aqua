81079340f5cd62c47088eb3a6046406c3c8d79e0
davithekid <byjdrk.27@gmail.com>
2025-11-14 16:00:00 -0300

    feat: inicializando dashboard sindico

---
+++ dashboard-sindico/src/app/layout.jsx
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Dashboard | Síndico",
+  description: "Dashboard destinada aos monitoramento dos sindicos sobre o seu condominio",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
