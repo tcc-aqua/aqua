9aea9353d716be14cf8542adfc51f1b76668a172
davithekid <byjdrk.27@gmail.com>
2025-12-02 09:35:28 -0300

    feat: consumo medio

--- backend-sindicos/src/controllers/RelatorioController.js
+++ backend-sindicos/src/controllers/RelatorioController.js
@@ -1,9 +1,17 @@
 import UserAlertService from "../services/relatorios/getConsumoAlto.js";
+import UserConsumoMedio from "../services/relatorios/getConsumoMensal.js";
+
 export default class RelatorioController {
-   
-    static async getUsersComConsumoAlto(req, reply){
-            const sindicoId = req.user.id; 
-            const consumo_alto = await UserAlertService.getUsersWithHighConsumptionAlert(sindicoId);
-            return reply.status(200).send({ quantidade: consumo_alto });
+
+    static async getUsersComConsumoAlto(req, reply) {
+        const sindicoId = req.user.id;
+        const consumo_alto = await UserAlertService.getUsersWithHighConsumptionAlert(sindicoId);
+        return reply.status(200).send({ quantidade: consumo_alto });
+    }
+
+    static async getConsumoMedio(req, reply) {
+        const sindicoId = req.user.id;
+        const consumoMedio = await UserConsumoMedio.getConsumoMedio(sindicoId);
+        return reply.status(200).send({ consumo_medio: consumoMedio });
     }
 }
--- backend-sindicos/src/routes/relatorios.routes.js
+++ backend-sindicos/src/routes/relatorios.routes.js
@@ -3,4 +3,5 @@ import RelatorioController from "../controllers/RelatorioController.js";
 
 export default async function relatorioRoutes(fastify){
     fastify.get('/consumo-alto',  {preHandler: autenticarSindico}, RelatorioController.getUsersComConsumoAlto)
+    fastify.get('/consumo-medio',  {preHandler: autenticarSindico}, RelatorioController.getConsumoMedio)
 }
